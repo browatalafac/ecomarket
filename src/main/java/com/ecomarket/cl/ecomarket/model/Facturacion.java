cd605e893ccc5d7d764b705e58652363e8cff61c
Melipilla <melipilla@duoc.acad>
2025-06-09 15:07:16 -0400

    Se agregó la clase Facturacion y este se rellenó.

---
+++ src/main/java/com/ecomarket/cl/ecomarket/model/Facturacion.java
@@ -0,0 +1,31 @@
+package com.ecomarket.cl.ecomarket.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name= "productos")
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Facturacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column (name = "nombre_cliente")
+    private String nombreCompletoCliente;
+
+    @Column (nullable = false)
+    private String descripcionProductos;
+
+    @Column (nullable = false)
+    private Double precioFinalPedido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "facturacion_id", nullable = false)
+    private Pedido pedido;
+}
